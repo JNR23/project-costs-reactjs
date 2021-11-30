9819b0f863405aafac25d65c9df65a3ab5b43aab
JNR23 <junior_bhzte@hotmail.com>
2021-11-30 20:05:31 -0300

    Component Input Select SubmitButton and update ProjectForm

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,18 @@
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+import styles from './ProjectForm.module.css';
+
+export default function ProjectForm({btnText}) {
+    return (
+        <form>
+            <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto"/>
+
+            <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira o orçamento total"/>
+
+            <Select name="category_id" text="Selecione a categoria" />
+
+            <SubmitButton text={btnText} />
+        </form>
+    );
+}
