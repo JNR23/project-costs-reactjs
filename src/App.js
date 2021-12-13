8848897d8fba3d7ec599940a4db3057f78d4d657
JNR23 <junior_bhzte@hotmail.com>
2021-12-13 18:28:30 -0300

    Dashboard

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Container from './components/layout/Container';
 import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
-
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import Home from './components/pages/Home';
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,3 +1,31 @@
+import { useLocation } from 'react-router';
+
+import Message from '../layout/Message';
+import Container from '../layout/Container';
+
+import styles from './Projects.module.css';
+import LinkButton from '../layout/LinkButton';
+
 export default function Projects() {
-    return <h1>Projects</h1>
+
+    const location = useLocation();
+
+    let message = '';
+
+    if (location.state) {
+        message = location.state.message;
+    }
+
+    return (
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" />                
+            </div>
+            {message && <Message type="success" msg={message} />}
+            <Container customClass="start">
+                <p>Projetos..</p>
+            </Container>
+        </div>
+    );
 }
